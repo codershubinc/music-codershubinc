a033d16a87fdc57f4a9f071097160348af003aa9
CodersHub-in <ingleswapnil2004@gmail.com>
2024-08-12 16:02:11 +0530

    full screen music player cmpl

--- src/app/play/musicPlayer.tsx
+++ src/app/play/musicPlayer.tsx
@@ -4,6 +4,7 @@ import music from '@/config/dataBase/playListsDb/music';
 import { useRouter } from 'next/navigation';
 import MediaSessionFunc from './mediaSession';
 import { handleSeek, pauseAudio, playAudio, player, playMusic, playNextTrack, playPreviousTrack } from './playControllers';
+import MusicPlayerFull from '@/components/cust/musicPlayerFullScreen';
 
 interface Props {
     musicIds: string[];
@@ -21,6 +22,7 @@ const MusicPlayer: React.FC<Props> = ({ musicIds, playMusicWithId, allMusicInfo 
     // ====> test useStates
     const [ppc, setPpc] = useState<string>()
     const [isPlaying, setIsPlaying] = useState(false);
+    const [fullScreen, setFullScreen] = useState(false)
 
 
     const router = useRouter()
@@ -82,6 +84,20 @@ const MusicPlayer: React.FC<Props> = ({ musicIds, playMusicWithId, allMusicInfo 
     };
     window.addEventListener('popstate', handleBackButton);
 
+    const handleFullScreen = (id: string) => {
+        if (id) {
+            const trackIndex = musicIds.indexOf(id);
+
+            if (trackIndex !== -1) {
+                // console.log('Setting initial track index:', trackIndex);
+                setCurrentTrackIndex(trackIndex);
+                setCurrentSongInfo(allMusicInfo[trackIndex]);
+                // console.log('Current song info:', currentSongInfo);
+            } else {
+                console.error('Track ID not found in musicIds:', id);
+            }
+        }
+    }
 
 
     // ==> media session api
@@ -98,41 +114,59 @@ const MusicPlayer: React.FC<Props> = ({ musicIds, playMusicWithId, allMusicInfo 
     }, [currentSongInfo])
     useEffect(() => { console.log('isPlaying', isPlaying) }, [isPlaying])
     return (
-        <div className={` w-[97%]  mx-auto fixed  bottom-0 left-0 right-0 rounded-xl bg-slate-950 p-2 m-2`}>
-
-            <div
-                className='flex justify-evenly'
-            >
-                <button onClick={() => playPreviousTrack(setCurrentTrackIndex, musicIds)}>👈</button>
-                <button onClick={() => playAudio(audioRef, setPpc, setCurrentTrackIndex, setIsPlaying)} >{ppc || 'play'}</button>
-                <button onClick={() => playNextTrack(setCurrentTrackIndex)}>⏭️</button>
-            </div>
-            {/* Seek bar */}
-            <input
-                type="range"
-                min="0"
-                max={duration}
-                value={currentTime}
-                onChange={(event) => handleSeek(event, audioRef, setCurrentTime)}
-                style={{ width: '100%' }}
+        fullScreen ?
+            <MusicPlayerFull
+                currentSongInfo={currentSongInfo}
+                isDisplay={fullScreen}
+                allMusicInfo={allMusicInfo}
+                playMusic={(id) => handleFullScreen(id)}
+                setIsDisplay={(val: boolean) => setFullScreen(val)}
+                plPaFn={() => playAudio(audioRef, setPpc, setCurrentTrackIndex, setIsPlaying)}
+                nextFn={() => playNextTrack(setCurrentTrackIndex)}
+                prevFn={() => playPreviousTrack(setCurrentTrackIndex, musicIds)}
+                seekFn={(event) => handleSeek(event, audioRef, setCurrentTime)}
+                duration={duration}
+                currentTime={currentTime}
+                isPlaying={isPlaying}
             />
-            <div className="flex flex-row  gap-5">
-                <p
-                    className='w-[70%] overflow-hidden text-nowrap'
-                >
-                    {currentSongInfo?.musicName || 'play the music'}
-                </p>
-
-                {/* <LikeBtn musicId={currentSongInfo?.$id} /> */}
+            :
+            <div className={` w-[97%]  mx-auto fixed  bottom-0 left-0 right-0 rounded-xl bg-slate-950 p-2 m-2`}>
 
                 <div
-                    className='w-[30%]'
+                    className='flex justify-evenly'
                 >
-                    {Math.floor(currentTime / 60)}:{('0' + Math.floor(currentTime % 60)).slice(-2)} /
-                    {Math.floor(duration / 60)}:{('0' + Math.floor(duration % 60)).slice(-2)}
+                    <button onClick={() => playPreviousTrack(setCurrentTrackIndex, musicIds)}>👈</button>
+                    <button onClick={() => playAudio(audioRef, setPpc, setCurrentTrackIndex, setIsPlaying)} >{ppc || 'play'}</button>
+                    <button onClick={() => playNextTrack(setCurrentTrackIndex)}>⏭️</button>
                 </div>
+                {/* Seek bar */}
+                <input
+                    type="range"
+                    min="0"
+                    max={duration}
+                    value={currentTime}
+                    onChange={(event) => handleSeek(event, audioRef, setCurrentTime)}
+                    style={{ width: '100%' }}
+                />
+                <div className="flex flex-row  gap-5">
+                    <p
+                        className='w-[70%] overflow-hidden text-nowrap'
+                    >
+                        {currentSongInfo?.musicName || 'play the music'}
+                    </p>
+
+                    {/* <LikeBtn musicId={currentSongInfo?.$id} /> */}
+
+                    <div
+                        className='w-[30%]'
+                    >
+                        {Math.floor(currentTime / 60)}:{('0' + Math.floor(currentTime % 60)).slice(-2)} /
+                        {Math.floor(duration / 60)}:{('0' + Math.floor(duration % 60)).slice(-2)}
+                    </div>
+                </div>
+                <button onClick={() => setFullScreen(true)} >dis</button>
             </div>
-        </div>
+
     );
 };
 
