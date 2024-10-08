ddc8023d2afe81f705f64ef34bf45ba94fa4a43c
Swapnil Ingle <ingleswapnil2004@gmail.com>
2024-10-09 01:05:13 +0530

    next song feature

--- src/components/cust/player/musicPlayerZero/musicPlayerFullZero.tsx
+++ src/components/cust/player/musicPlayerZero/musicPlayerFullZero.tsx
@@ -1,9 +1,6 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../../../ui/button'
-import DecodeHTMLEntities from '@/utils/func/htmlDecode'
-import { Pause, Play, SkipBack, SkipForward } from 'lucide-react';
-import AddToPlayList from '../../userActions/addToPlayList';
 import KeepScreenAwake from '../../screenAwake/keepAwake';
 import Avatar from './avatar';
 import Controllers from './controlleres';
@@ -48,13 +45,35 @@ function MusicPlayerFullZero(
     }: MusicPlayerProps
 ) {
 
+    // console.log('all music info', allMusicInfo);
+
+    const [nextSongInfo, setNextSongInfo] = useState<SongInfo | null>(null);
+
+    useEffect(() => {
+        console.log('playListId', playListId);
+        const currentIndex = allMusicInfo?.indexOf(currentSongInfo)
+
+        if (currentIndex) {
+            console.log('currentIndex', currentIndex);
+
+
+            const nextIndex = currentIndex === -1 ? 1 : (currentIndex + 1) % allMusicInfo.length
+
+            console.log('nextSongInfo', allMusicInfo[nextIndex]);
+            setNextSongInfo(allMusicInfo[nextIndex])
+        }
+
+
+
+
+    }, [currentSongInfo])
+
     return (
         <div
             className='w-screen h-screen flex flex-col justify-center items-center fixed top-0 left-0 right-0 bottom-0 z-50 bg-black'
-
         >
             <div
-            className='flex w-screen h-screen   justify-evenly items-center'
+                className='flex w-screen h-screen   justify-evenly items-center'
             >
                 <Avatar currentSongInfo={currentSongInfo} />
                 <Controllers
@@ -74,6 +93,21 @@ function MusicPlayerFullZero(
 
             {/* // ! other controls */}
             <KeepScreenAwake />
+            <p>Up Next</p>
+            <div
+            className='flex text-center justify-center items-center gap-2 border border-white rounded-full p-2 mt-2 animate-aurora'
+            >
+                <img
+                    src={nextSongInfo?.musicAvatarUrl}
+                    alt=""
+                    className='w-14 h-14 rounded-full border      '
+                />
+                <p
+                className='text-white text-xl font-semibold'
+                >
+                    {nextSongInfo?.musicName}
+                </p>
+            </div>
             <Button
                 className='text-3xl bg-slate-950 text-white fixed top-0 right-0 rotate-180 md:right-3 lg:right-4 '
                 onClick={() => setIsDisplay(false)}
