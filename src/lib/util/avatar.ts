e6c875201ccc1ba64e3c1598ec83e956ffe4c2c7
Swapnil Ingle <ingleswapnil2004@gmail.com>
2024-11-12 19:49:54 +0530

    creating playlist bug fix

--- src/lib/util/avatar.ts
+++ src/lib/util/avatar.ts
@@ -6,14 +6,9 @@ const asyncHandler = (fn: any) => (req?: any, res?: any, next?: any) => {
 // A utility function to fetch avatar initials as a standalone async function
 const getAvatarInitials = async (name: string): Promise<string> => {
     try {
-        const ftc = await fetch(`https://api-codershubinc.vercel.app/v0.1/random_image/t/initials/${name}/svg`);
-        const data = await ftc.json();
 
-        // Log the response to confirm data structure
-        console.log('image initial ', data?.data?.imageUrl);
 
-        // Return the image URL if available, otherwise a default string
-        return data?.data?.imageUrl+'&backgroundColor=000000' || 'default-avatar-url';
+        return `https://api.dicebear.com/9.x/initials/svg?seed=${name}&backgroundColor=000000  `
     } catch (error) {
         console.error('Error fetching avatar initials:', error);
         throw error;  // Let the calling code handle this error
